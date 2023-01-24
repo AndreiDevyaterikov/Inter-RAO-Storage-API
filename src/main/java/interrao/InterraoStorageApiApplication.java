9b418653cb40b58e95ec6169caa2af8722325859
AndreiDevyaterikov <devyaterikov.andrei@gmail.com>
2023-01-24 15:49:23 +0300

    init project

---
+++ src/main/java/interrao/InterraoStorageApiApplication.java
@@ -0,0 +1,13 @@
+package interrao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterraoStorageApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterraoStorageApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/interrao/InterraoStorageApiApplicationTests.java
@@ -0,0 +1,13 @@
+package interrao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterraoStorageApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
